1fa0797cb02f2a243b5b3d042d0496a983e41ad3
mniko238 <mniko238@mtroyal.ca>
2022-12-16 08:08:36 -0700

    motion images animation speed

--- globals.py
+++ globals.py
@@ -1,3 +1,5 @@
+from threading import Timer
+
 ######################################################################
 # VARIABLES
 
@@ -11,3 +13,14 @@ YELLOW_COLOR=(255, 255, 0)
 GREEN_COLOR=(0, 255, 255)
 ORANGE_COLOR=(255, 100, 0)
 BLUE_COLOR = (0, 0, 128)
+
+# https://stackoverflow.com/a/48741004
+# Used to regulate the speed of motion images. 
+# This timer works in the background (thread) and calls the approriate function every x seconds
+# timer = RepeatTimer(intSecs, funcNameToRun)
+# timer.start() | timer.cancel()
+# timer.cancel()
+class RepeatTimer(Timer):
+    def run(self):
+        while not self.finished.wait(self.interval):
+            self.function(*self.args, **self.kwargs)
--- helicopter.py
+++ helicopter.py
@@ -10,17 +10,17 @@ class Helicopter():
         self.screen = screen
         self.spritesDict = spritesDict
 
-        self.imageCount = 0
         self.image = None
         self.rect = None
 
+        self.nextImgNum = 0
         self.set_image()
+
+        self.nextImageTimer = globals.RepeatTimer(0.04, self.set_image)
+        self.nextImageTimer.start()
 
     ######################################################################
     # OPTIONAL
-
-    def update_location(self):
-        self.set_image()
 
     def blit(self):
         self.screen.blit(self.image, self.rect)
@@ -31,12 +31,12 @@ class Helicopter():
     def set_image(self):
         while True:
             try:
-                self.image = pygame.image.load(f"./media/images/helicopter/{self.imageCount}.gif")
+                self.image = pygame.image.load(f"./media/images/helicopter/{self.nextImgNum}.gif")
                 break
             except:
-                self.imageCount = 0
+                self.nextImgNum = 0
 
-        self.imageCount += 1
+        self.nextImgNum += 1
 
         self.rect = self.image.get_rect()
         self.rect.move_ip(20, 20)
